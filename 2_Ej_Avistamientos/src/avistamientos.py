c047ed0cef42a8660e5aa1b7ca1b149d74a320ed
JORGE GARCIA GUTIERREZ <jorgarcia@us.es>
2022-10-13 13:16:48 +0200

    Subidas para el próximo miércoles 19

--- 2_Ej_Avistamientos/src/avistamientos.py
+++ 2_Ej_Avistamientos/src/avistamientos.py
@@ -32,3 +32,78 @@ def lee_avistamientos(fichero):
             longitud = float(longitud)
             result.append(Avistamiento(fechahora, ciudad, estado, forma, duracion, comentarios, latitud, longitud)) 
     return result
+
+# Función auxiliar para el cálculo de la distancia entre dos coordenadas
+def distancia(coordenadas1, coordenadas2):
+    '''
+    Devuelve la distancia entre dos coordenadas.
+    
+    ENTRADA:
+       - coordenadas1: tupla con latitud y longitud -> (float, float)
+       - coordenadas2: tupla con latitud y longitud -> (float, float)     
+    SALIDA:
+       - distancia entre las dos coordenadas -> float
+    '''
+    return sqrt((coordenadas2[0] - coordenadas1[0])**2
+                     + (coordenadas2[1] - coordenadas1[1])**2)
+
+def avistamientos_cercanos_ubicacion(avistamientos, ubicacion, r):
+    ''' 
+    Devuelve el conjunto de avistamientos cercanos a una ubicación.
+    
+    ENTRADA:
+       - avistamientos: lista de tuplas con la información de los avistamientos
+            -> [Avistamiento(datetime, str, str, str, int, str, float, float)]
+       - ubicacion: coordenadas de la ubicación para la cual queremos encontrar
+         avistamientos cercanos -> (float, float)
+       - r: radio de distancia -> float
+    SALIDA:
+       - Conjunto de avistamientos que se encuentran a una distancia
+         inferior al valor "r" de la ubicación dada por el parámetro "ubicacion" 
+            -> {Avistamiento(datetime, str, str, str, int, str, float, float)}
+    '''
+    pass
+
+def numero_avistamientos_fecha(avistamientos, fecha):
+    ''' Avistamientos que se han producido en una fecha
+    
+    ENTRADA: 
+       - avistamientos: lista de avistamientos
+            -> [Avistamiento(datetime, str, str, str, int, str, float, float)]
+       - fecha: fecha del avistamiento -> datetime.date
+    SALIDA: 
+       - Número de avistamientos producidos en la fecha -> int
+    
+    Toma como entrada una lista de avistamientos y una fecha.
+    Devuelve el número de avistamientos que se han producido en esa fecha.
+    '''
+    pass
+
+def formas_estados(avistamientos, estados):
+    ''' 
+    Devuelve el número de formas distintas observadas en avistamientos 
+    producidos en alguno de los estados especificados.
+    
+    ENTRADA:
+       - avistamientos: lista de tuplas con la información de los avistamientos
+            -> [Avistamiento(datetime, str, str, str, int, str, float, float)]
+       - estados: conjunto de estados para los que se quiere hacer el cálculo -> {str}
+    SALIDA:
+       - Número de formas distintas observadas en los avistamientos producidos
+         en alguno de los estados indicados por el parámetro "estados" -> int
+    '''
+    pass
+
+def duracion_total(avistamientos, estado):
+    ''' 
+    Devuelve la duración total de los avistamientos de un estado. 
+    
+    ENTRADA:
+       - avistamientos: lista de tuplas con la información de los avistamientos 
+            -> [Avistamiento(datetime, str, str, str, int, str, float, float)]
+       - estado: estado para el que se quiere hacer el cálculo -> str
+    SALIDA:
+       - duración total en segundos de todos los avistamientos del estado -> int
+    '''
+    pass
+
--- 2_Ej_Avistamientos/src/test_avistamientos.py
+++ 2_Ej_Avistamientos/src/test_avistamientos.py
@@ -1,5 +1,24 @@
 from avistamientos import *
 
+def test_duracion_total(avistamientos):
+    # Test de la función duracion_total
+    for estado in ['in', 'nm', 'pa', 'wa']:
+        print("Duración total de los avistamientos en {}: {} segundos."
+            .format(estado, duracion_total(avistamientos, estado)))
+
+def test_formas_estados(avistamientos):
+    # Test de la función formas_estados
+    conjunto_estados = {'in', 'nm', 'pa', 'wa'}
+    print("Número de formas distintas observadas en los estados {}: {}"
+      .format(', '.join(conjunto_estados), formas_estados(avistamientos, conjunto_estados)))
+
+def test_numero_avistamientos_fecha(avistamientos):
+    # Test de la función numero_avistamientos_fecha
+    fecha = datetime(2005, 5, 1).date()
+    numero_avistamientos = numero_avistamientos_fecha(avistamientos, fecha)
+    print("El día {} se produjeron {} avistamientos"
+        .format(datetime.strftime(fecha, '%d/%m/%Y'), numero_avistamientos))
+
 def main():
     DATOS = lee_avistamientos('./data/ovnis.csv')
     print(DATOS[0])
